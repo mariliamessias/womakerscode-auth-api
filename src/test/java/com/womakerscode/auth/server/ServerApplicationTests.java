992867f71e0f1c5f4d2df2eb54e0aaf13de5c3ae
mariliamessias <marilia.oliveira@outlook.com>
2022-04-24 15:18:14 -0300

    initial commit

---
+++ src/test/java/com/womakerscode/auth/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.womakerscode.auth.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
